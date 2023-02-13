bd21362f124bf2372e16beb6d087c2a3245ab05d
VictorRevers <60626825+VictorRevers@users.noreply.github.com>
2023-02-13 13:32:51 -0300

    Update javaDataTypes.java

--- javaDataTypes.java
+++ javaDataTypes.java
@@ -21,17 +21,17 @@ class Solution{
 										
                 System.out.println(x+" can be fitted in:");
                 if(x>=-128 && x<=127){
-					System.out.println("* byte");
-				}
+			System.out.println("* byte");
+		}
                 if(x>=-32768 && x<= 32767){
-					System.out.println("* short");
-				}
-				if(x>=-(Math.pow(2,31)) && x<=(Math.pow(2, 31)-1)){
-					System.out.println("* int");
-				}
-				if(x>=-(Math.pow(2,63)) && x<=(Math.pow(2, 63)-1)){
-					System.out.println("* long");
-				}
+			System.out.println("* short");
+		}
+		if(x>=-(Math.pow(2,31)) && x<=(Math.pow(2, 31)-1)){
+			System.out.println("* int");
+		}
+		if(x>=-(Math.pow(2,63)) && x<=(Math.pow(2, 63)-1)){
+			System.out.println("* long");
+		}
             }
             catch(Exception e)
             {
@@ -39,7 +39,7 @@ class Solution{
             }
 
         }
-		sc.close();
+	sc.close();
     }
 }
 
