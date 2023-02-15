e0c3aed82968de63e152c51645170ad7241b3b04
VictorRevers <60626825+VictorRevers@users.noreply.github.com>
2023-02-15 16:18:20 -0300

    Update JavaEnd-of-file.java

--- JavaEnd-of-file.java
+++ JavaEnd-of-file.java
@@ -6,16 +6,16 @@ class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 		
-		String str;
-		int i = 0;
+	String str;
+	int i = 0;
 		
-		while(sc.hasNext()){
-			str = sc.nextLine();
-			i++;
-			System.out.printf("%d %s %n", i, str);				
-		}
+	while(sc.hasNext()){
+		str = sc.nextLine();
+		i++;
+		System.out.printf("%d %s %n", i, str);				
+	}
 				
-		sc.close();
+	sc.close();
 		
 		
     }
